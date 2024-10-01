8f817f99e54fa35bb3fc7924905e2fa5ebebf1ca
Richard Jiménez <richardmartus@gmail.com>
2024-10-01 13:50:44 -0300

    Cambio de BD a Mysql y creación de superuser

--- Sensores/settings.py
+++ Sensores/settings.py
@@ -77,11 +77,15 @@ WSGI_APPLICATION = 'Sensores.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'sensoriot',
+            'USER': 'root',
+            'PASSWORD': 'root',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
